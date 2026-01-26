1045dad47a06e246973bd51df8289710902aca5c
BigCelery842906 <84037295+BigCelery842906@users.noreply.github.com>
2026-01-26 14:50:18 +0000

    Added a base for the particle model
    It won't do anything differently yet, there is nothing worthwhile in it just yet

--- DX11PhysicsFramework/ParticleModel.cpp
+++ DX11PhysicsFramework/ParticleModel.cpp
@@ -1,5 +1,24 @@
 ﻿#include "ParticleModel.h"
 
-ParticleModel::ParticleModel(Transform* transform) : PhysicsModel(transform)
+ParticleModel::ParticleModel(Transform* transform, float resetTime, Vector3 pertubation, bool invertGravity) : PhysicsModel(transform)
 {
+    _resetTime = resetTime;
+    _pertubation = pertubation;
+    _invertGravity = invertGravity;
+}
+
+void ParticleModel::Reset()
+{
+    // Reset Logic here
+}
+
+void ParticleModel::Update(float deltaTime)
+{
+    _aliveTime += deltaTime;
+    if (_aliveTime > _resetTime)
+    {
+        Reset();
+    }
+    
+    PhysicsModel::Update(deltaTime);
 }
--- DX11PhysicsFramework/ParticleModel.h
+++ DX11PhysicsFramework/ParticleModel.h
@@ -3,8 +3,18 @@
 
 class ParticleModel : public PhysicsModel
 {
+private:
+    float _resetTime;
+    float _aliveTime;
+    
+    bool _invertGravity;
+    Vector3 _pertubation;
 public:
-    ParticleModel(Transform* transform);
+    ParticleModel(Transform* transform) : PhysicsModel(transform) {};
+    ParticleModel(Transform* transform, float resetTime, Vector3 pertubation, bool invertGravity);
+
+    void Reset();
+    void Update(float deltaTime) override;
     
     
 };
